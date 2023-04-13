a1c0b0ec87dc7fd0eba71e75c57eaebc239a7412
Ales <aleswebgit@gmail.com>
2023-04-13 08:47:48 +0200

    🔧 reset i estils base preview d'storybook

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,7 @@
 import type { Preview } from "@storybook/react";
+import 'minireset.css'
+import '../src/styles/tokens.css'
+import '../src/styles/global.css'
 
 const preview: Preview = {
   parameters: {
