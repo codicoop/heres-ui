eeb086d526392f80595a6b9bc5867cfefce7c98d
Ales <aleswebgit@gmail.com>
2023-04-13 13:04:27 +0200

    📝 story Text component

---
+++ src/stories/Text/Text.stories.ts
@@ -0,0 +1,38 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Text } from './Text';
+
+const meta: Meta<typeof Text> = {
+  title: 'UI-base/Text',
+  component: Text,
+  tags: ['autodocs'],
+  argTypes: {
+    variant: {
+      control: {
+        type: 'select',
+      },
+    },
+    extra: {
+      control: {
+        type: 'boolean',
+      },
+    }
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof Text>;
+
+export const Default: Story = {
+  args: {
+    children: 'Text',
+    extra: false,
+  },
+};
+
+export const Heading: Story = {
+  args: {
+    ...Default.args,
+    variant: 'title',
+  },
+};
