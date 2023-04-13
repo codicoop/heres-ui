ac41d01fd438072b5277f9439670e8eea5caeb1f
Ales <aleswebgit@gmail.com>
2023-04-13 14:37:36 +0200

    📝🚚 separació de components a les stories

--- src/stories/Button/Button.stories.ts
+++ src/stories/Button/Button.stories.ts
@@ -3,7 +3,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 import { Button } from './Button';
 
 const meta: Meta<typeof Button> = {
-  title: 'UI-base/Button',
+  title: 'Molecules/Button',
   component: Button,
   tags: ['autodocs'],
   argTypes: {
--- src/stories/Text/Text.stories.ts
+++ src/stories/Text/Text.stories.ts
@@ -3,7 +3,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 import { Text } from './Text';
 
 const meta: Meta<typeof Text> = {
-  title: 'UI-base/Text',
+  title: 'Typography/Text',
   component: Text,
   tags: ['autodocs'],
   argTypes: {
