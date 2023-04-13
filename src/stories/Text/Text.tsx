79e179b2cdb2fc0bbfda4e318c3ecf98de48462b
Ales <aleswebgit@gmail.com>
2023-04-13 13:21:02 +0200

    🩹 variació Text per defecte body

--- src/stories/Text/Text.tsx
+++ src/stories/Text/Text.tsx
@@ -3,7 +3,7 @@ import './Text.css'
 
 export interface TextProps {
   children: React.ReactNode,
-  variant: 'body' | 'title' | 'subtitle',
+  variant?: 'body' | 'title' | 'subtitle',
   extra?: boolean,
   as?: 'p' | 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6',
   className?: string,
