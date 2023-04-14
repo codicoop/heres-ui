849fa77d74477fbb07db4a6b7ccbfbf6a3b7a04f
Ales <aleswebgit@gmail.com>
2023-04-14 11:33:56 +0200

    🌱 creat GoToSections component

---
+++ src/stories/Cards/GoToSections/GoToSections.tsx
@@ -0,0 +1,38 @@
+import { Text } from '../../Typography/Text/Text'
+import './GoToSections.css'
+
+export interface section {
+  name: string,
+  url: string,
+  color: 'primary' | 'secondary' | 'tertiary' | 'gray' | 'white',
+  description?: string,
+}
+
+export interface GoToSectionsProps {
+  sections: section[],
+}
+
+export function GoToSections ({
+  sections = [],
+}: GoToSectionsProps): JSX.Element {
+  return (
+    <div className="go-to-sections">
+      {sections?.map((section) => (
+        <section 
+          key={section.name}
+          className={`go-to-sections--item item--${section.color}`}
+        >
+          <Text variant='subtitle' as='h2'>
+            {section.name}
+          </Text>
+
+          {section.description && (
+            <Text variant='body' as='p'>
+              {section.description}
+            </Text>
+          )}
+        </section>
+      ))}
+    </div>
+  )
+}
