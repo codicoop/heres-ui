2a9b9b7fec80a4d44ca8b28add3b283878ecafad
Ales <aleswebgit@gmail.com>
2023-04-14 09:04:59 +0200

    🌱 creat Input component

---
+++ src/stories/Forms/Input/Input.tsx
@@ -0,0 +1,38 @@
+import './Input.css'
+
+export interface InputProps {
+  label: string,
+  name: string,
+  placeholder?: string,
+  type?: 'text' | 'email' | 'password',
+  value?: string,
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void,
+  className?: string,
+}
+
+export function Input ({
+  label,
+  name,
+  placeholder,
+  type = 'text',
+  value,
+  onChange,
+  className,
+  ...props
+}: InputProps): JSX.Element {
+  return (
+    <div className={`form--item`}>
+      <label htmlFor={name}>{label}</label>
+      <input
+        id={name}
+        name={name}
+        type={type}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+        {...props}
+        className={className}
+      />
+    </div>
+  )
+}
