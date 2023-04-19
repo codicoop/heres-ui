db9384850cfeef6752f41e8e1651d01fbd0b1865
Ales <aleswebgit@gmail.com>
2023-04-19 15:57:48 +0200

    🌱 pàgina Login

---
+++ src/pages/Auth/Login.tsx
@@ -0,0 +1,39 @@
+import { Link, useNavigate } from "react-router-dom";
+import { Input } from "../../stories/Forms/Input/Input";
+import { Title } from "../../stories/Typography/Title/Title";
+import { Button } from "../../stories/Button/Button";
+import { useContext } from "react";
+import { WireframesContext } from "../../config/WireframesContext";
+
+export default function Login (): JSX.Element {
+  const { setAuth } = useContext(WireframesContext)
+  const navigate = useNavigate()
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault()
+    console.log(e)
+    setAuth(true)
+    navigate('/app/inici')
+  }
+  return (
+    <>
+      <Title underline>Iniciar Sessió</Title>
+      <form onSubmit={handleSubmit}>
+        <Input label="Email" type="email" name="email" />
+        <Input label="Contrasenya" type="password" name="password" />
+        <div>
+          <Link to="/app/recoverpass">
+            Recuperar contrasenya
+          </Link>
+          <Button
+            type="submit"
+            label="Iniciar Sessió"
+          />
+        </div>
+        <Link to="/app/register/0" className="button button--base">
+          Registrar-se
+        </Link>
+      </form>
+    </>
+  )
+}
