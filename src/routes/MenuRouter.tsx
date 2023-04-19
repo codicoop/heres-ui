bc5d0cb1790d11e334396b8b939c9c1fa155063b
Ales <aleswebgit@gmail.com>
2023-04-19 15:21:20 +0200

    ✨ context de rol al menu del router (extern)

--- src/routes/MenuRouter.tsx
+++ src/routes/MenuRouter.tsx
@@ -1,24 +1,35 @@
 import { Link } from "react-router-dom"
 import { routesWireframes } from "./routes"
+import { useContext } from "react"
+import { WireframesContext } from "../config/WireframesContext"
 
 export default function MenuRouter (): JSX.Element {
+  const { role } = useContext(WireframesContext)
+  
+  const roleAcces = (route: any): boolean => {
+    return route.role.includes(role) || route.role.includes('all')
+  }
+
   return (
     <nav>
       <ul className='menu'>
         {routesWireframes
-          // anivellar les rutes, de tal manera que també s'imprimeixin les rutes de les subrutes
           .map((route) => (
-            route.children ?
-              route.children.map((subroute: any) => (
-                <li key={subroute.path}>
-                  <Link to={`${route.path}/${subroute.path}`}>
-                    {subroute.name}
-                  </Link>
+            roleAcces(route) && (
+              route.children ?
+                route.children.map((subroute: any) => (
+                  roleAcces(subroute) && (
+                    <li key={subroute.path}>
+                      <Link to={`${route.path}/${subroute.path}`}>
+                        {subroute.name}
+                      </Link>
+                    </li>
+                  )
+                )) :
+                <li key={route.path}>
+                  <Link to={route.path}>{route.name}</Link>
                 </li>
-              )) :
-              <li key={route.path}>
-                <Link to={route.path}>{route.name}</Link>
-              </li>
+            )  
           ))
         }
       </ul>
