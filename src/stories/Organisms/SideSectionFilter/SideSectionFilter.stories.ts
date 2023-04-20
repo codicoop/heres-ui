df89d31729579fd5a968c1a48e00a14172c030b3
Ales <aleswebgit@gmail.com>
2023-04-20 21:26:27 +0200

    📝 args d'exemple a SideSectionFilter

--- src/stories/Organisms/SideSectionFilter/SideSectionFilter.stories.ts
+++ src/stories/Organisms/SideSectionFilter/SideSectionFilter.stories.ts
@@ -7,6 +7,35 @@ const meta: Meta<typeof SideSectionFilter> = {
   component: SideSectionFilter,
   tags: ['autodocs'],
   args: {
+    title: 'Bones pràctiques',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum beatae consequatur maxime temporibus necessitatibus enim eaque inventore, omnis, dolorum assumenda dignissimos voluptate soluta sapiente asperiores, laboriosam velit repudiandae harum architecto nisi earum!',
+    filters: [
+      {
+        name: "Any",
+        value: "year",
+        options: ["Tots", "2021", "2020", "2019"],
+        color: "gray",
+      },
+      {
+        name: "Metodologia",
+        value: "methodology",
+        options: ["Tots", "Metodologia 1", "Metodologia 2", "Metodologia 3"],
+        color: "primary",
+      },
+      {
+        name: "Sector",
+        value: "sector",
+        options: ["Tots", "Sector 1", "Sector 2", "Sector 3"],
+        color: "white",
+      },
+      {
+        name: "Tema",
+        value: "theme",
+        options: ["Tots", "Tema 1", "Tema 2", "Tema 3"],
+        color: "secondary",
+      },
+    ],
   },
   argTypes: {
   },
