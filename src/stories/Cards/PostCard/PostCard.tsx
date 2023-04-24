b3ee34232bad776e492c15d8000b5095ccb53d63
Ales <aleswebgit@gmail.com>
2023-04-24 23:50:50 +0200

    🐛 contingut de PostCard

--- src/stories/Cards/PostCard/PostCard.tsx
+++ src/stories/Cards/PostCard/PostCard.tsx
@@ -1,12 +1,23 @@
+import { Text } from '../../Typography'
 import './PostCard.css'
 
 export interface PostCardProps {
+  title: string
+  summary?: string
 }
 
 export function PostCard ({
+  title,
+  summary,
 }: PostCardProps) {
   return (
-    <>
-    </>
+    <article className='post-card'>
+      <Text  variant='subtitle' as='h2'>
+        {title}
+      </Text>
+      <Text>
+        {summary}
+      </Text>
+    </article>
   )
 }
