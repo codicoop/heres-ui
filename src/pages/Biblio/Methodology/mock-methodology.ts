b80ec7d4009c836164f3f2c34a7bf7a2f47dbc88
Ales <aleswebgit@gmail.com>
2023-04-25 00:25:54 +0200

    🤡 dades per info de metodologies

---
+++ src/pages/Biblio/Methodology/mock-methodology.ts
@@ -0,0 +1,54 @@
+import { SideSectionFilterProps } from "../../../stories/Organisms/SideSectionFilter/SideSectionFilter"
+
+export const methodologies = [
+  {
+    id: 'methodology-1',
+    title: 'Methodology Card',
+    summary: 'This is a methodology card. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde similique tempore iure!',
+  },
+  {
+    id: 'methodology-2',
+    title: 'Methodology Card 2',
+    summary: 'This is a methodology card 2. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde similique tempore iure!',
+  },
+  {
+    id: 'methodology-3',
+    title: 'Methodology Card 3',
+    summary: 'This is a methodology card 3. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde similique tempore iure!',
+  },
+  {
+    id: 'methodology-4',
+    title: 'Methodology Card 4',
+    summary: 'This is a methodology card 4. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde similique tempore iure!',
+  },
+  {
+    id: 'methodology-5',
+    title: 'Methodology Card 5',
+    summary: 'This is a methodology card 5. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde similique tempore iure!',
+  },
+  {
+    id: 'methodology-6',
+    title: 'Methodology Card 6',
+    summary: 'This is a methodology card 6. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde similique tempore iure!',
+  },
+]
+
+export const infoSideSectionFilter: SideSectionFilterProps = {
+  title: "Metodologies",
+  description:
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem asperiores fuga nesciunt adipisci? Neque rerum eius labore sint minus illo perspiciatis alias tempore doloremque earum. Quas ad eius ratione totam optio debitis!",
+  filters: [
+    {
+      name: "Sector",
+      value: "sector",
+      options: ["Tots", "Sector 1", "Sector 2", "Sector 3"],
+      color: "white",
+    },
+    {
+      name: "Tema",
+      value: "theme",
+      options: ["Tots", "Tema 1", "Tema 2", "Tema 3"],
+      color: "secondary",
+    },
+  ],
+}
