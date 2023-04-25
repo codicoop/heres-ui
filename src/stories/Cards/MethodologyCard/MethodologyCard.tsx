32f8533304b14fd3575f35276756dfd4d821f321
Ales <aleswebgit@gmail.com>
2023-04-25 09:55:05 +0200

    ✨ us d'Icon a la Card de Metodologia

--- src/stories/Cards/MethodologyCard/MethodologyCard.tsx
+++ src/stories/Cards/MethodologyCard/MethodologyCard.tsx
@@ -1,3 +1,6 @@
+import { useState } from 'react'
+import { Icon } from '../../Atoms'
+import { IconName } from '../../Atoms/Icon/svgs'
 import { Text } from '../../Typography'
 import './MethodologyCard.css'
 
@@ -10,11 +13,27 @@ export function MethodologyCard ({
   title,
   summary,
 }: MethodologyCardProps) {
+  const [iconSwitch, setIconSwitch] = useState<IconName>('bookmarkOutline')
+
+  const handleIconSwitch = () => {
+    if (iconSwitch === 'bookmarkOutline') setIconSwitch('bookmark')
+    if (iconSwitch === 'bookmark') setIconSwitch('bookmarkOutline')
+  }
+
   return (
     <article className='methodology-card'>
-      <Text  variant='subtitle' as='h2'>
-        {title}
-      </Text>
+      <header>
+        <Text  variant='subtitle' as='h2'>
+          {title}
+        </Text>
+        <Icon
+          name={iconSwitch}
+          onClick={handleIconSwitch}
+          selectedColor='primary'
+          hoverColor='primary'
+          {...(iconSwitch === 'bookmark' && { selected: true })}
+        />
+      </header>
       <Text>
         {summary}
       </Text>
