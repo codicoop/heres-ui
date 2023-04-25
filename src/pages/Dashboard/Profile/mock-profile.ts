4ca0aa97acf550796acdf606bcb2515626213d80
Ales <aleswebgit@gmail.com>
2023-04-25 10:55:29 +0200

    🤡 mock perfils (org i user)

---
+++ src/pages/Dashboard/Profile/mock-profile.ts
@@ -0,0 +1,23 @@
+export const orgInfo = {
+  "Mail": "org@correu.com",
+  "Correu de contacte": "org@correu.com",
+  "Nom comercial": "Org",
+  "Nom fiscal": "Org",
+  "NIF": "A12345678",
+  "Adreça de la seu social": "Carrer de la Ciutat de Granada, 1, 08003 Barcelona",
+  "Sector": "Sector 1",
+  "Nombre de persones que treballen a l'organització": "1-10",
+}
+
+export const userInfo = {
+  "Nom": "Usuari",
+  "Cognoms": "Usuari",
+  "Mail": "usuari@correu.com",
+  "DNI": "12345678A",
+  "Data de naixement": "01/01/1990",
+  "Municipi de residència": "Barcelona",
+  "Codi postal": "08003",
+  "Telèfon de contacte": "123456789",
+  "Perfil professional": "Treballant per compte aliè",
+  "Organització": "Org",
+}
