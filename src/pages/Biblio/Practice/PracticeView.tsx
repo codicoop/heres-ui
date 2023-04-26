8e3fcac4aa0aa80141fb1dda7f1c566363f3139b
Ales <aleswebgit@gmail.com>
2023-04-26 13:26:02 +0200

    ✨ practica individual poder tornar a llistat

--- src/pages/Biblio/Practice/PracticeView.tsx
+++ src/pages/Biblio/Practice/PracticeView.tsx
@@ -1,18 +1,31 @@
+import { Link } from "react-router-dom";
 import { ContainerWithAside } from "../../../stories/Layouts";
+import { JazzyAction } from "../../../stories/Molecules";
 import { SideSectionInfo } from "../../../stories/Organisms";
 import { practiceInfo } from "./mock-practice";
 
 export default function PracticeView (): JSX.Element {
   return (
-    <ContainerWithAside>
-      <SideSectionInfo
-        generalInfoSection={practiceInfo.generalInfoSection}
-        relatedType={practiceInfo.relatedType}
-        relatedItems={practiceInfo.relatedItems}
-      />
-      <main>
-        <h1>Practice View</h1>
-      </main>
-    </ContainerWithAside>
+    <>
+      <Link
+        to="/app/practiques"
+      >
+        <JazzyAction
+          text="Tornar a la llista de pràctiques"
+          icon="arrowLeftThick"
+          iconSide="left"
+        />
+      </Link>
+      <ContainerWithAside>
+        <SideSectionInfo
+          generalInfoSection={practiceInfo.generalInfoSection}
+          relatedType={practiceInfo.relatedType}
+          relatedItems={practiceInfo.relatedItems}
+        />
+        <main>
+          <h1>Practice View</h1>
+        </main>
+      </ContainerWithAside>
+    </>
   )
 }
