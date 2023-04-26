78627ce27adc2a8f2656ba1357c6eaee09e9f1df
Ales <aleswebgit@gmail.com>
2023-04-26 17:13:48 +0200

    🌱 contingut a la vista de post

--- src/pages/Biblio/News/PostView.tsx
+++ src/pages/Biblio/News/PostView.tsx
@@ -1,7 +1,38 @@
+import { Link } from "react-router-dom";
+import { JazzyAction } from "../../../stories/Molecules";
+import { ContainerWithAside } from "../../../stories/Layouts";
+import { ContentBlock, SideSectionInfo } from "../../../stories/Organisms";
+import { postContent, postInfo } from "./mock-news";
+
 export default function PostView (): JSX.Element {
   return (
     <>
-      PostView
+      <Link
+        to="/app/noticies"
+      >
+        <JazzyAction
+          text="Tornar a la llista de notícies"
+          icon="arrowLeftThick"
+          iconSide="left"
+        />
+      </Link>
+      <ContainerWithAside>
+        <SideSectionInfo
+          generalInfoSection={postInfo.generalInfoSection}
+          relatedType={postInfo.relatedType}
+          relatedItems={postInfo.relatedItems}
+        />
+        <main>
+          {postContent?.map((content, index) => (
+            <ContentBlock
+              key={`content-${index}`}
+              text={content.text}
+              img={content.img}
+              imgSide={content.imgSide}
+            />
+          ))}
+        </main>
+      </ContainerWithAside>
     </>
   )
 }
