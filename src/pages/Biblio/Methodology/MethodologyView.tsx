77a8bc8a56757ad9aef282922ae00b53a11d9db9
Ales <aleswebgit@gmail.com>
2023-04-26 16:59:15 +0200

    🌱 contingut a la vista de metodologia

--- src/pages/Biblio/Methodology/MethodologyView.tsx
+++ src/pages/Biblio/Methodology/MethodologyView.tsx
@@ -1,7 +1,38 @@
+import { Link } from "react-router-dom";
+import { JazzyAction } from "../../../stories/Molecules";
+import { ContainerWithAside } from "../../../stories/Layouts";
+import { ContentBlock, SideSectionInfo } from "../../../stories/Organisms";
+import { methodologyContent, methodologyInfo } from "./mock-methodology";
+
 export default function MethodologyView (): JSX.Element {
   return (
     <>
-      MethodologyView
+      <Link
+        to="/app/metodologies"
+      >
+        <JazzyAction
+          text="Tornar a la llista de metodologies"
+          icon="arrowLeftThick"
+          iconSide="left"
+        />
+      </Link>
+      <ContainerWithAside>
+        <SideSectionInfo
+          generalInfoSection={methodologyInfo.generalInfoSection}
+          relatedType={methodologyInfo.relatedType}
+          relatedItems={methodologyInfo.relatedItems}
+        />
+        <main>
+          {methodologyContent?.map((content, index) => (
+            <ContentBlock
+              key={`content-${index}`}
+              text={content.text}
+              img={content.img}
+              imgSide={content.imgSide}
+            />
+          ))}
+        </main>
+      </ContainerWithAside>
     </>
   )
 }
