5134c2114b20d296afaf3c5d57a0e9117c0e6f43
MartaRF3 <ricartfm@gmail.com>
2023-04-27 11:17:06 +0200

    Maquetat menú dinàmic

--- src/pages/Base/Inici.tsx
+++ src/pages/Base/Inici.tsx
@@ -1,24 +1,21 @@
 import { Link } from "react-router-dom";
 import { Text, Title } from "../../stories/Typography";
 import { GoToSection } from "../../stories/Cards";
+import './inici.css'
 
 export default function Inici (): JSX.Element {
   return (
-    <>
-      <div>
-        <Text as="h2">Eina d'autodiagnosi</Text>
+    <div className="inici page">
+      <div className="inici__intro container container--blue">
+        <img src="../../public/imatge_prova.png" alt="" />
+        <Title>Eina d'Autodiagnosi sobre Impacte</Title>
+        <svg className="arrow" width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M4 10.5V14.5H13L9.5 18L11.92 20.42L19.84 12.5L11.92 4.58002L9.5 7.00002L13 10.5H4Z" fill="#4F4F4F"/>
+        </svg>
       </div>
-      <div>
-        <Title>
-          Nom de l'aplicació
-        </Title>
+      <div className="inici__text container">
         <Text>
-          Descripció de l'aplicació.
-        </Text>
-        <Text>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias distinctio esse, vel odit cum, optio ipsam ratione eius dolore quas architecto similique autem vitae perferendis culpa?
-        </Text>
-        <Text>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est natus asperiores, veritatis ullam iste non itaque!
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est natus asperiores, veritatis ullam iste non itaque!
         </Text>
       </div>
@@ -52,6 +49,6 @@ export default function Inici (): JSX.Element {
           />
         </Link>
       </section>
-    </>
+    </div>
   )
 }
