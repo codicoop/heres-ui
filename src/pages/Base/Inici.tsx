33f5782364f23affc412e25bcffdd1c33e3b6d80
MartaRF3 <ricartfm@gmail.com>
2023-04-27 12:04:01 +0200

    Pàgina inici maquetada

--- src/pages/Base/Inici.tsx
+++ src/pages/Base/Inici.tsx
@@ -1,17 +1,24 @@
 import { Link } from "react-router-dom";
 import { Text, Title } from "../../stories/Typography";
 import { GoToSection } from "../../stories/Cards";
+import { Icon } from "../../stories/Atoms";
 import './inici.css'
 
 export default function Inici (): JSX.Element {
   return (
     <div className="inici page">
-      <div className="inici__intro container container--blue">
-        <img src="../../public/imatge_prova.png" alt="" />
+      <div className="inici__intro">
+        <div className="inici__img">
+          <img src="../../public/imatge_prova.png" alt="" />
+        </div>
         <Title>Eina d'Autodiagnosi sobre Impacte</Title>
-        <svg className="arrow" width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M4 10.5V14.5H13L9.5 18L11.92 20.42L19.84 12.5L11.92 4.58002L9.5 7.00002L13 10.5H4Z" fill="#4F4F4F"/>
-        </svg>
+        <div className="inici__arrow">
+          <Icon
+            name="arrowRightThick"
+            selectedColor="primary"
+            hoverColor="primary"
+          />
+        </div>
       </div>
       <div className="inici__text container">
         <Text>
