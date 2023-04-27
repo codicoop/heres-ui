1f53877a411122c97e4b467a1837b5f62aa6271b
Ales <aleswebgit@gmail.com>
2023-04-27 10:19:24 +0200

    🌱 base pàg PracticeAdd

--- src/pages/Biblio/index.ts
+++ src/pages/Biblio/index.ts
@@ -1,6 +1,7 @@
 export { default as Biblio } from './Biblio';
 export { default as MethodologyList } from './Methodology/MethodologyList';
 export { default as MethodologyView } from './Methodology/MethodologyView';
+export { default as PracticeAdd } from './Practice/PracticeAdd';
 export { default as PracticeList } from './Practice/PracticeList';
 export { default as PracticeView } from './Practice/PracticeView';
 export { default as NewsList } from './News/NewsList';
