23dbfd466d8f6cf065d773cdfa28b9142a0a0c1d
Ales <aleswebgit@gmail.com>
2023-04-28 04:59:33 +0200

    ✨ pàg Perfil

--- src/pages/Dashboard/Profile/Profile.tsx
+++ src/pages/Dashboard/Profile/Profile.tsx
@@ -1,15 +1,38 @@
 import { useContext } from "react"
 import { WireframesContext } from "../../../config/WireframesContext"
 import { Title } from "../../../stories/Typography"
+import { InfoSection } from "../../../stories/Molecules"
+import { userInfo } from "./mock-profile"
+import { LayoutDashboard } from "../../../components"
 
 export default function Profile () {
   const { role } = useContext(WireframesContext)
 
   return (
-    <>
-      <Title underline>
-        Perfil de l'{role}
-      </Title>
-    </>
+    <LayoutDashboard>
+      <>
+        <Title underline>
+          Perfil de l'{role}
+        </Title>
+        {role === 'user' && (
+          Object.values(userInfo).map((info, index) => (
+            <InfoSection
+              key={`info-user-${index}`}
+              category={Object.keys(userInfo)[index]}
+              data={info}
+            />
+          ))
+        )}
+        {role === 'org' && (
+          Object.values(userInfo).map((info, index) => (
+            <InfoSection
+              key={`info-org-${index}`}
+              category={Object.keys(userInfo)[index]}
+              data={info}
+            />
+          ))
+        )}
+      </>
+    </LayoutDashboard>
   )
 }
