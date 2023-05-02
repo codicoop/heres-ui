c54372886cccbffaa94cad2d6376b827270693c4
Ales <aleswebgit@gmail.com>
2023-05-02 15:44:23 +0200

    📝 Metodologia story amb ambit i sectors

--- src/stories/Cards/MethodologyCard/MethodologyCard.stories.ts
+++ src/stories/Cards/MethodologyCard/MethodologyCard.stories.ts
@@ -9,6 +9,9 @@ const meta: Meta<typeof MethodologyCard> = {
   args: {
     title: 'Methodology Title on Card',
     summary: 'Methodology summary on Card. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquam repudiandae neque fuga.',
+    saved: false,
+    ambit: 'Social',
+    sectors: ["Energétic", "Equips electronics", "Telecomunicacions i tecnologies de la informació"]
   },
   argTypes: {
   },
@@ -18,3 +21,21 @@ export default meta;
 type Story = StoryObj<typeof MethodologyCard>;
 
 export const Default: Story = {};
+
+export const OneSector: Story = {
+  args: {
+    sectors: ["Energétic"]
+  }
+};
+
+export const TwoSectors: Story = {
+  args: {
+    sectors: ["Energétic", "Equips electronics"]
+  }
+};
+
+export const MoreThanTwoSectors: Story = {
+  args: {
+    sectors: ["Energétic", "Equips electronics", "Telecomunicacions i tecnologies de la informació"]
+  }
+};
