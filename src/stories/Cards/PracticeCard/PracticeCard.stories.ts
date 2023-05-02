74611c195a655b4968e6cad714b6c0a6e2ecdc4b
Ales <aleswebgit@gmail.com>
2023-05-02 11:17:15 +0200

    📝 Pràctica story amb ambit, sector, metodologia

--- src/stories/Cards/PracticeCard/PracticeCard.stories.ts
+++ src/stories/Cards/PracticeCard/PracticeCard.stories.ts
@@ -8,7 +8,10 @@ const meta: Meta<typeof PracticeCard> = {
   tags: ['autodocs'],
   args: {
     title: 'Practice Card',
-    summary: 'This is a practice card',
+    summary: 'This is a practice card. Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat sed voluptates, nobis iure veniam animi magni assumenda eos ipsam quos. Tenetur, error alias?',
+    ambit: 'Social',
+    sector: 'Sector social',
+    methodology: 'Metodologia 1',
   },
   argTypes: {
   },
