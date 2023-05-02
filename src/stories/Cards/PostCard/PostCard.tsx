9ac87dd0f57e79af4534028b9dff485a73bffc2a
Ales <aleswebgit@gmail.com>
2023-05-02 11:32:16 +0200

    ✨ Post rep ambit, sector i metodolgia

--- src/stories/Cards/PostCard/PostCard.tsx
+++ src/stories/Cards/PostCard/PostCard.tsx
@@ -4,20 +4,56 @@ import './PostCard.css'
 export interface PostCardProps {
   title: string
   summary?: string
+  ambit: string
+  sector: string
+  methodology?: string
 }
 
 export function PostCard ({
   title,
   summary,
+  ambit,
+  sector,
+  methodology,
 }: PostCardProps) {
   return (
     <article className='post-card'>
-      <Text  variant='subtitle' as='h2'>
-        {title}
-      </Text>
+      <header className='post-card__header'>
+        <Text  variant='subtitle' as='h2' extra>
+          {title}
+        </Text>
+      </header>
       <Text>
         {summary}
       </Text>
+      <footer className='post-card__footer'>
+        <div className='post-card__footer--info post-card__ambit'>
+          <Text size='small'>
+            {ambit}
+          </Text>
+          <Text as='h3' variant="body" extra>
+            Àmbit
+          </Text>
+        </div>
+        <div className='post-card__footer--info post-card__sector'>
+          <Text size='small'>
+            {sector}
+          </Text>
+          <Text as='h3' variant="body" extra>
+            Sector
+          </Text>
+        </div>
+        {methodology && (
+          <div className='post-card__footer--info post-card__methodology'>
+            <Text size='small'>
+              {methodology}
+            </Text>
+            <Text as='h3' variant="body" extra>
+              Metodologia de referència
+            </Text>
+          </div>
+        )}
+      </footer>
     </article>
   )
 }
