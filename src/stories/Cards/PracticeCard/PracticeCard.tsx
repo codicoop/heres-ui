d18bd278e0535ec1308d6a9d6423d091dcddec71
Ales <aleswebgit@gmail.com>
2023-05-02 11:16:49 +0200

    ✨ Pràctica rep ambit, sector i metodolgia

--- src/stories/Cards/PracticeCard/PracticeCard.tsx
+++ src/stories/Cards/PracticeCard/PracticeCard.tsx
@@ -3,21 +3,55 @@ import './PracticeCard.css'
 
 export interface PracticeCardProps {
   title: string
-  summary?: string
+  summary: string
+  ambit: string
+  sector: string
+  methodology: string
 }
 
 export function PracticeCard ({
   title,
   summary,
+  ambit,
+  sector,
+  methodology,
 }: PracticeCardProps) {
   return (
     <article className='practice-card'>
-      <Text  variant='subtitle' as='h2'>
-        {title}
-      </Text>
+      <header className='practice-card__header'>
+        <Text  variant='subtitle' as='h2' extra>
+          {title}
+        </Text>
+      </header>
       <Text>
         {summary}
       </Text>
+      <footer className='practice-card__footer'>
+        <div className='practice-card__footer--info practice-card__ambit'>
+          <Text size='small'>
+            {ambit}
+          </Text>
+          <Text as='h3' variant="body" extra>
+            Àmbit
+          </Text>
+        </div>
+        <div className='practice-card__footer--info practice-card__sector'>
+          <Text size='small'>
+            {sector}
+          </Text>
+          <Text as='h3' variant="body" extra>
+            Sector
+          </Text>
+        </div>
+        <div className='practice-card__footer--info practice-card__methodology'>
+          <Text size='small'>
+            {methodology}
+          </Text>
+          <Text as='h3' variant="body" extra>
+            Metodologia de referència
+          </Text>
+        </div>
+      </footer>
     </article>
   )
 }
