2ac1d340dcd27b4f1c21d22ea13a3d6f36faf931
Ales <aleswebgit@gmail.com>
2023-05-09 18:24:26 +0200

    ✨ formacio a Inici enviar a login si no es user

--- src/pages/Base/Inici.tsx
+++ src/pages/Base/Inici.tsx
@@ -1,10 +1,21 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { WireframesContext } from "../../config/WireframesContext";
 import { Text, Title } from "../../stories/Typography";
 import { GoToSection } from "../../stories/Cards";
 import { Icon } from "../../stories/Atoms";
 import './inici.css'
 
 export default function Inici (): JSX.Element {
+  const { auth, role } = useContext(WireframesContext)
+
+  const accesToFormations = () => {
+    if (auth) {
+      if (role === 'user') return '/app/formacio'
+      return '/app/nomes-usuaris'
+    }
+    return '/app/login'
+  }
   return (
     <div className="inici page">
       <div className="inici__container">
@@ -42,7 +53,7 @@ export default function Inici (): JSX.Element {
               description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae."
             />
           </Link>
-          <Link to="/app/formacio">
+          <Link to={accesToFormations()}>
             <GoToSection
               name="Formació"
               color="secondary"
